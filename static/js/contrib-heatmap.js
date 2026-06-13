76ad1afe6648fe4e3ef10f10ca0436c3aa288a54
CaptainStinkRat <tatejonathan182@gmail.com>
2026-06-13 04:41:49 -0500

    profile pic back to 120px; add subtle fade-in/hover animations; fix contrib graph to fetch+inline SVG

--- static/js/contrib-heatmap.js
+++ static/js/contrib-heatmap.js
@@ -6,14 +6,26 @@
   var loading = document.getElementById('contrib-loading');
   var chartUrl = 'https://ghchart.rshah.org/' + username;
 
-  var img = new Image();
-  img.onload = function() {
-    if (loading) loading.style.display = 'none';
-    img.classList.add('contrib-img-loaded');
-    container.appendChild(img);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', chartUrl, true);
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      if (loading) loading.style.display = 'none';
+      var wrapper = document.createElement('div');
+      wrapper.innerHTML = xhr.responseText;
+      var svg = wrapper.querySelector('svg');
+      if (svg) {
+        svg.style.maxWidth = '100%';
+        svg.style.height = 'auto';
+        svg.style.borderRadius = '6px';
+        container.appendChild(svg);
+      }
+    } else {
+      if (loading) loading.textContent = 'Could not load contribution graph.';
+    }
   };
-  img.onerror = function() {
+  xhr.onerror = function() {
     if (loading) loading.textContent = 'Could not load contribution graph.';
   };
-  img.src = chartUrl;
+  xhr.send();
 })();
